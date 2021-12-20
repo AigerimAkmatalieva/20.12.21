42a3637f9d34cc05e87607887899b93129bcb228
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2021-12-20 15:34:10 +0600

    script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ alert("Hello World");
 
 console.log("Hello world");
 
-var firstName, age, middleName,lastName, street, years, country;
+var firstName, age, middleName, lastName, street, years, country, Mybirthday;
 
 
 var lastName = "Volkov", middleName = "Vasilivich";
 
-firstName = "Vasya" 
+firstName = "Vasya"
 console.log(firstName);
 age = "5"
 console.log(age);
@@ -23,3 +23,17 @@ years = "23";
 console.log(years);
 country = "Kyrgyzstan";
 console.log(country);
+Mybirthday = "21.08.03";
+console.log(Mybirthday);
+
+
+var potatoes = 5, tomatoes = 3;
+var vegetables = potatoes + tomatoes;
+console.log(vegetables);
+
+var result = vegetables / 2;
+console.log(result);
+
+var result = (2 + 1) / 3 + 6 * 2;
+console.log(result);
+
